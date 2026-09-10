3151e0bbe03a469fbaa4be33dd6583efaf447f5b
oogunishou-art <oogunishou@gmail.com>
2026-09-11 06:01:33 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+/* トレlog service worker
+   Caches the app shell so it works offline once opened.
+   Bump CACHE when you change files to force an update. */
+const CACHE = 'tore-log-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.open(CACHE).then(cache =>
+      cache.match(e.request).then(hit => {
+        const net = fetch(e.request).then(res => {
+          if (res && res.status === 200 && res.type === 'basic') {
+            cache.put(e.request, res.clone());
+          }
+          return res;
+        }).catch(() => hit);
+        return hit || net;
+      })
+    )
+  );
+});
